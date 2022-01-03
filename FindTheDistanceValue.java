5b92b1878007e4672190a9485cdf6edb88906dfe
Inzamamul Haque <54953508+inzamamul-haque@users.noreply.github.com>
2022-01-03 22:10:07 +0600

    Find The Distance Value

---
+++ FindTheDistanceValue.java
@@ -0,0 +1,18 @@
+public class FindTheDistanceValue {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int distance = 0;
+        for(int i=0; i<arr1.length; i++) {
+            boolean flag = true;
+            for(int j=0; j<arr2.length; j++) {
+                if(Math.abs(arr1[i]-arr2[j]) <= d) {
+                  flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                distance++;
+            }
+        }
+        return distance;
+    }
+}
